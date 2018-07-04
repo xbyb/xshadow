28d92e6cb75a1eb42a37e8d61909a36b70de8f1f
xbyb <1009202339@qq.com>
2018-07-05 00:16:15 +0800

    初始工程

---
+++ xsite/src/main/java/com/xshadow/it/xsite/SiteForXshadowApplication.java
@@ -0,0 +1,12 @@
+package com.xshadow.it.xsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteForXshadowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteForXshadowApplication.class, args);
+	}
+}
